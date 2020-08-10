91633d54a787decb438105f7891721bbb64d1528
Dmitry Tebryaev <tebr5923@mail.ru>
2020-08-10 15:53:51 +0700

    refactored RacerLap

--- src/main/java/com/foxminded/racer/RacerLap.java
+++ src/main/java/com/foxminded/racer/RacerLap.java
@@ -1,21 +1,13 @@
 package com.foxminded.racer;
 
-import java.time.Duration;
 import java.time.LocalTime;
 
 public class RacerLap {
     private final Racer racer;
-    private final RacerTime startTime;
-    private final RacerTime endTime;
     private final LocalTime racerResult;
 
-    public RacerLap(Racer racer, RacerTime startTime, RacerTime endTime) {
+    public RacerLap(Racer racer, LocalTime racerResult) {
         this.racer = racer;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.racerResult = LocalTime
-                .ofNanoOfDay(Duration
-                        .between(endTime.getLocalDateTime(), startTime.getLocalDateTime())
-                        .toNanos());
+        this.racerResult = racerResult;
     }
 }
---
+++ src/main/java/com/foxminded/racer/RacerResult.java
@@ -0,0 +1,15 @@
+package com.foxminded.racer;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class RacerResult {
+    // might be static?
+    LocalTime compute(LocalDateTime endTime, LocalDateTime startTime) {
+        return LocalTime
+                .ofNanoOfDay(Duration
+                        .between(endTime, startTime)
+                        .toNanos());
+    }
+}
