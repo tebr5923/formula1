434913e068547d7c40d887991aae4b03f08f0739
Dmitry Tebryaev <tebr5923@mail.ru>
2020-08-26 16:49:09 +0700

    extract Reader impl to a constructor and del private methods read and parse

--- src/main/java/com/foxminded/racer/RacerLapsFactory.java
+++ src/main/java/com/foxminded/racer/RacerLapsFactory.java
@@ -14,40 +14,28 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class RacerLapsFactory {
     private final Parser<Racer> racerParser;
     private final Parser<RacerTime> timeParser;
+    private final Reader reader;
 
     public RacerLapsFactory() {
         this.racerParser = new RacerParser();
         this.timeParser = new TimeParser();
+        this.reader = new ResourceFileReader();
     }
 
     public List<RacerLap> create(String abbreviationFileName, String startTimeFileName, String endTimeFileName) throws IOException, URISyntaxException {
-        Map<String, Racer> racers = parseRacer(read(abbreviationFileName));
-        Map<String, RacerTime> startTimes = parseRacerTime(read(startTimeFileName));
-        Map<String, RacerTime> endTimes = parseRacerTime(read(endTimeFileName));
+        Map<String, Racer> racers = racerParser.parse(reader.read(abbreviationFileName));
+        Map<String, RacerTime> startTimes = timeParser.parse(reader.read(startTimeFileName));
+        Map<String, RacerTime> endTimes = timeParser.parse(reader.read(endTimeFileName));
 
         return racers
                 .values()
                 .stream()
                 .map(racer -> createRacerLap(racer, startTimes, endTimes))
                 .collect(Collectors.toList());
-    }
-
-    private Stream<String> read(String fileName) throws IOException, URISyntaxException {
-        Reader reader = new ResourceFileReader();
-        return reader.read(fileName);
-    }
-
-    private Map<String, Racer> parseRacer(Stream<String> stringStreamRacers) {
-        return racerParser.parse(stringStreamRacers);
-    }
-
-    private Map<String, RacerTime> parseRacerTime(Stream<String> stringStreamTimes) {
-        return timeParser.parse(stringStreamTimes);
     }
 
     private RacerLap createRacerLap(Racer racer, Map<String, RacerTime> startTimes, Map<String, RacerTime> endTimes) {
