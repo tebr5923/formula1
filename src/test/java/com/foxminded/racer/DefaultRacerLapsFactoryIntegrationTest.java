dc97589af7f40c66a88bd3c2a8134cb0b64a860a
Dmitry Tebryaev <tebr5923@mail.ru>
2020-08-26 19:00:47 +0700

    rename DefaultRacerLapsFactoryTest

---
+++ src/test/java/com/foxminded/racer/DefaultRacerLapsFactoryIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.foxminded.racer;
+
+import com.foxminded.util.RacerLapBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DefaultRacerLapsFactoryIntegrationTest {
+
+    @Test
+    void create_shouldReturnCorrectResult() throws IOException, URISyntaxException {
+        List<RacerLap> expected = new ArrayList<>();
+        RacerLapBuilder racerLapBuilder = new RacerLapBuilder();
+        expected.add(racerLapBuilder.build("SVF", "Sebastian Vettel", "FERRARI", LocalTime.of(0, 1, 4, 415000000)));
+        expected.add(racerLapBuilder.build("FAM", "Fernando Alonso", "MCLAREN RENAULT", LocalTime.of(0, 1, 12, 657000000)));
+
+        RacerLapsFactory racerLapsFactory = new DefaultRacerLapsFactory();
+        List<RacerLap> actual = racerLapsFactory.create("abbreviations.txt", "start.log", "end.log");
+
+        assertEquals(expected, actual);
+    }
+}
--- src/test/java/com/foxminded/racer/DefaultRacerLapsFactoryTest.java
+++ src/test/java/com/foxminded/racer/DefaultRacerLapsFactoryTest.java
@@ -1,28 +1,22 @@
 package com.foxminded.racer;
 
-import com.foxminded.util.RacerLapBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultRacerLapsFactoryTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
     @Test
-    void create_shouldReturnCorrectResult() throws IOException, URISyntaxException {
-        List<RacerLap> expected = new ArrayList<>();
-        RacerLapBuilder racerLapBuilder = new RacerLapBuilder();
-        expected.add(racerLapBuilder.build("SVF", "Sebastian Vettel", "FERRARI", LocalTime.of(0, 1, 4, 415000000)));
-        expected.add(racerLapBuilder.build("FAM", "Fernando Alonso", "MCLAREN RENAULT", LocalTime.of(0, 1, 12, 657000000)));
-
-        RacerLapsFactory racerLapsFactory = new DefaultRacerLapsFactory();
-        List<RacerLap> actual = racerLapsFactory.create("abbreviations.txt", "start.log", "end.log");
-
-        assertEquals(expected, actual);
+    void create() {
     }
 }
