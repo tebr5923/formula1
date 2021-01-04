4537d2c51b7ad4247f3a5bf6b672d4096b287338
Dmitry Tebryaev <tebr5923@mail.ru>
2021-01-04 22:26:08 +0700

    add qualifiedPosition in RacerLapFormatterRacerLapWithDelimiter

--- src/main/java/com/foxminded/formatter/RacerLapFormatterRacerLapWithDelimiter.java
+++ src/main/java/com/foxminded/formatter/RacerLapFormatterRacerLapWithDelimiter.java
@@ -8,17 +8,24 @@ import java.util.List;
 
 public class RacerLapFormatterRacerLapWithDelimiter extends FormatterRacerLapDecorator {
     private static final int FIRST_ROUND_QUALIFIED_POSITION = 15;
+    private final int qualifiedPosition;
 
     public RacerLapFormatterRacerLapWithDelimiter(Formatter<RacerLap> racerLapFormatter) {
         super(racerLapFormatter);
+        this.qualifiedPosition = FIRST_ROUND_QUALIFIED_POSITION;
+    }
+
+    public RacerLapFormatterRacerLapWithDelimiter(Formatter<RacerLap> racerLapFormatter, int qualifiedPosition) {
+        super(racerLapFormatter);
+        this.qualifiedPosition = qualifiedPosition;
     }
 
     @Override
     public List<String> format(List<RacerLap> inputList) {
         List<String> stringList = super.format(inputList);
         int maxStringLength = stringList.stream().max(Comparator.comparing(String::length)).toString().length();
-        if (FIRST_ROUND_QUALIFIED_POSITION <= stringList.size()) {
-            stringList.add(FIRST_ROUND_QUALIFIED_POSITION, repeatChar('-',maxStringLength));
+        if (qualifiedPosition <= stringList.size()) {
+            stringList.add(qualifiedPosition, repeatChar('-',maxStringLength));
         }
         return stringList;
     }
