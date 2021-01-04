b237389e85ffc2d483bfdb9667e55ccfd83b9009
Dmitry Tebryaev <tebr5923@mail.ru>
2021-01-04 22:20:56 +0700

    add empty RacerLapFormatterRacerLapWithDelimiterTest

---
+++ src/test/java/com/foxminded/formatter/RacerLapFormatterRacerLapWithDelimiterTest.java
@@ -0,0 +1,12 @@
+package com.foxminded.formatter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacerLapFormatterRacerLapWithDelimiterTest {
+
+    @Test
+    void format() {
+    }
+}
