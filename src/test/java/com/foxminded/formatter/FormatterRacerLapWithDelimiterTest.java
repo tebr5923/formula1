b086c24e6fa6b17929c2448a7909f3b73818ea94
Dmitry Tebryaev <tebr5923@mail.ru>
2021-01-05 16:41:54 +0700

    del RacerLapBuilder from FormatterRacerLapWithDelimiterTest

--- src/test/java/com/foxminded/formatter/FormatterRacerLapWithDelimiterTest.java
+++ src/test/java/com/foxminded/formatter/FormatterRacerLapWithDelimiterTest.java
@@ -1,7 +1,6 @@
 package com.foxminded.formatter;
 
 import com.foxminded.racer.RacerLap;
-import com.foxminded.util.RacerLapBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,22 +35,14 @@ class FormatterRacerLapWithDelimiterTest {
         expected.add("4. Daniel Ricciardo | RED BULL RACING TAG HEUER | 01:12.658");
         expected.add("5. Lewis Hamilton   | MERCEDES                  | 01:12.659");
 
-        List<RacerLap> racerLapList = new ArrayList<>();
-        RacerLapBuilder racerLapBuilder = new RacerLapBuilder();
-        racerLapList.add(racerLapBuilder.build("LHM", "Lewis Hamilton", "MERCEDES", LocalTime.of(0, 1, 12, 659000000)));
-        racerLapList.add(racerLapBuilder.build("FAM", "Fernando Alonso", "MCLAREN RENAULT", LocalTime.of(0, 1, 12, 657000000)));
-        racerLapList.add(racerLapBuilder.build("DRR", "Daniel Ricciardo", "RED BULL RACING TAG HEUER", LocalTime.of(0, 1, 12, 658000000)));
-        racerLapList.add(racerLapBuilder.build("SVF", "Sebastian Vettel", "FERRARI", LocalTime.of(0, 1, 4, 415000000)));
-        racerLapList.add(racerLapBuilder.build("KRF", "Kimi Raikkonen", "FERRARI", LocalTime.of(0, 1, 12, 655000000)));
-
         FormatterRacerLapWithDelimiter formatterRacerLapWithDelimiter =
                 new FormatterRacerLapWithDelimiter(mockRacerLapFormatter, 3);
-        List<String> actual = formatterRacerLapWithDelimiter.format(racerLapList);
+        List<String> actual = formatterRacerLapWithDelimiter.format(new ArrayList<>());
 
         assertEquals(expected, actual);
     }
 
-    private void initMockRacerLapFormatter(){
+    private void initMockRacerLapFormatter() {
         List<String> result = new ArrayList<>();
         result.add("1. Sebastian Vettel | FERRARI                   | 01:04.415");
         result.add("2. Kimi Raikkonen   | FERRARI                   | 01:12.655");
